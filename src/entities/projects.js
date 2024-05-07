e39fe45fbb0216fd0051fa281a79c1beac3b6fe9
김아름 <89237300+BeautyKim@users.noreply.github.com>
2024-05-07 15:48:23 +0900

    2024년 포트폴리오 entitie 수정

--- src/entities/projects.js
+++ src/entities/projects.js
@@ -6,14 +6,14 @@ const projects = [
     date: "2024.05 ~ 2024.05",
     cover: "./cover/portfolio.png",
     image:
-      "https://github.com/BeautyKim/EEAaO-landing/raw/master/images/readme/%ED%99%94%EB%A9%B4.gif",
+      "https://github.com/BeautyKim/portfolio_2024/raw/main/public/view.gif",
     description:
-      "HTML, CSS 및 JavaScript를 복습하기 위해 최소한으로 외부 라이브러리 사용해 개발",
-    tack: "Carousel, Scroll snap, 가로 마우스휠 스크롤이벤트, keyframes, lottiefiles",
-    env: "React18",
+      "React18 연습 및 2024년까지의 프로젝트를 한번에 정리하기 위하여 개발",
+    tack: "useState(), useRef(), useEffect() 및 map()을 활용한 UI",
+    env: "React18, Node.js v20, Scss",
     gitUrl: "https://github.com/BeautyKim/portfolio_2024",
     notionUrl: "",
-    url: "",
+    url: "https://www.beauty-kim.site",
   },
   {
     id: 3,
